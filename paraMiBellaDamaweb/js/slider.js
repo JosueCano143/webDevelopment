419d57ca990384c32da94915a3c7e2d0c265806d
Josue Cano <44412494+JosueCano143@users.noreply.github.com>
2020-10-09 18:33:48 -0500

    Add files via upload

---
+++ paraMiBellaDamaweb/js/slider.js
@@ -0,0 +1,41 @@
+var slider = $('#slider');
+var anterior = $('#btn-prev');
+var siguiente = $('#btn-next');
+
+$('#slider section:last').insertBefore('#slider section:first');
+
+slider.css('margin-left', '-' + 100 + '%');
+
+function moverD(){
+	slider.animate({
+		marginLeft:'-'+200+'%'
+	} ,700, function(){
+		$('#slider section:first').insertAfter('#slider section:last');
+		slider.css('margin-left', '-' + 100 + '%');
+	});
+}
+
+function moverI(){
+	slider.animate({
+		marginLeft:0
+	} ,700, function(){
+		$('#slider section:last').insertBefore('#slider section:first');
+		slider.css('margin-left', '-' + 100 + '%');
+	});
+}
+
+siguiente.on('click',function(){
+	moverD();
+});
+
+anterior.on('click',function(){
+	moverI();
+});
+
+function autoplay(){
+	interval = setInterval(function(){
+		moverD();
+	}, 6000);
+}
+
+autoplay();
